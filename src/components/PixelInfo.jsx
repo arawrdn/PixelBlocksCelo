f429c30f941c5cfee98b68930bf8a896b57a17af
0xward <aradeawardana97@gmail.com>
2025-10-02 10:00:50 +0700

    Create PixelInfo.jsx

---
+++ src/components/PixelInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PixelInfo() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      Click on a pixel to change its color. All actions are recorded on Celo testnet.
+    </div>
+  );
+}
